6b474cceab8dc964072651517bad4538ec695fdc
AnhNguyen20695 <felixnguyen1520@gmail.com>
2021-12-17 01:24:32 +0800

    rm package locl

--- front/ui/src/App.js
+++ front/ui/src/App.js
@@ -12,14 +12,6 @@ function App() {
         <p>
           Yeu em nhieu nhieu
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
